21f4fd8e078efa645c5ada26ef5d1c9259b95f7a
Ben Gould <ben+dev@amb1ent.org>
2022-04-05 15:18:51 +0200

    pass some parameters xml -> err0 appender

--- src/main/java/io/err0/log4j2/Err0Appender.java
+++ src/main/java/io/err0/log4j2/Err0Appender.java
@@ -5,10 +5,8 @@ import org.apache.logging.log4j.core.Core;
 import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
-import org.apache.logging.log4j.core.config.plugins.Plugin;
-import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
-import org.apache.logging.log4j.core.config.plugins.PluginElement;
-import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.config.plugins.*;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -16,21 +14,50 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * With thanks to https://www.baeldung.com/log4j2-plugins
  */
 @Plugin(name = "Err0Appender",
-        category = Core.CATEGORY_NAME,
-        elementType = Appender.ELEMENT_TYPE)
+        category = Core.CATEGORY_NAME)
 public class Err0Appender extends AbstractAppender {
+
+    @PluginBuilderFactory
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder implements org.apache.logging.log4j.core.util.Builder<Err0Appender> {
+
+        @PluginBuilderAttribute("name")
+        @Required
+        private String name;
+
+        @PluginBuilderAttribute("token")
+        @Required
+        private String token;
+
+        @PluginBuilderAttribute("url")
+        @Required
+        private String url;
+
+        // ... additional properties
+
+        // ... getters and setters
+
+        @Override
+        public Err0Appender build() {
+            return new Err0Appender(name, null);
+        }
+    }
+
 
     private static ConcurrentLinkedQueue<LogEvent> queue = new ConcurrentLinkedQueue<>();
 
     protected Err0Appender(String name, Filter filter) {
         super(name, filter, null);
     }
-
+/*
     @PluginFactory
     public static Err0Appender createAppender(@PluginAttribute("name") String name, @PluginElement("Filter") final Filter filter) {
         // TODO: pickup configuration for appender, or throw an exception
         return new Err0Appender(name, filter);
-    }
+    }*/
 
     @Override
     public void append(LogEvent event) {
